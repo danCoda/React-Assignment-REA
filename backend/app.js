d783000e7f6e990a06dbbca6e5e1fc61635f32ea
Daniel Son <dson.email@gmail.com>
2021-07-04 10:27:30 +1000

    chore:backend route done, can respond with data.

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,32 @@
-console.log("Hello");
+/*
+Backend code.
+- Runs on port 5000.
+- One route: '/api/data' (GET request) for getting data.
+
+How to start:
+- 'node app' in a command prompt. 
+*/
 
 const express = require('express');
-const cors = require('cors');
-const bodyParser = require('body-parser');
+const cors = require('cors'); 
+const bodyParser = require('body-parser'); 
+const fs = require('fs'); // For reading the json data file.
+
+const app = express();
+const PORT_NUMBER = 5000;
+const BASE_URL = '/api';
+const DATA = JSON.parse(fs.readFileSync(`${__dirname}/data.json`, 'utf8'));
+
+app.use(express.urlencoded({
+    extended: true
+})); // Be able to parse req.body.
+app.use(cors()); // Allow us to communicate with our locally-run frontend.
+app.use(bodyParser.json()); // Allow us to process incoming request bodies.
+
+app.get(`${BASE_URL}/data`, (req, res) => {
+    res.send(DATA);
+});
+
+app.listen(PORT_NUMBER, () => {
+    console.log(`Backend running on port ${PORT_NUMBER}!`);
+});
